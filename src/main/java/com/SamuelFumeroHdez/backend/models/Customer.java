738e526ab372801f6406725a22e64c98513280cd
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2021-10-23 00:24:21 +0100

    Creacion del customer y modificacion del user

---
+++ src/main/java/com/SamuelFumeroHdez/backend/models/Customer.java
@@ -0,0 +1,44 @@
+package com.SamuelFumeroHdez.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(
+        schema = "erp",
+        name = "customer"
+)
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(name = "CUSTOMER_GEN", sequenceName = "CUSTOMER_USER", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUSTOMER_GEN")
+    private Long customerId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "legalname", nullable = false)
+    private String legalName;
+
+    @Column(name = "cif", nullable = false)
+    private String cif;
+
+    @Email
+    @NotBlank(message = "Email is required")
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    /*private Address address;*/ /** Pendiente **/
+    /*private List<Invoice> invoices*/ /** Pendiente **/
+    /*private String email*/ /** Pendiente **/
+    /*private List<SubCompany> subcompanies*/
+
+}
--- src/main/java/com/SamuelFumeroHdez/backend/models/User.java
+++ src/main/java/com/SamuelFumeroHdez/backend/models/User.java
@@ -12,7 +12,10 @@ import java.time.Instant;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "users")
+@Table(
+        schema = "erp",
+        name = "users"
+)
 @Entity
 public class User {
     @Id
@@ -21,17 +24,30 @@ public class User {
     private Long userId;
 
     @NotBlank(message = "Username is required")
+    @Column(name = "username", nullable = false)
     private String username;
 
     @NotBlank(message = "Password is required")
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @NotBlank(message = "Name is required")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotBlank(message = "Lastname is required")
+    @Column(name = "lastname", nullable = false)
+    private String lastName;
 
     @Email
     @NotBlank(message = "Email is required")
+    @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "creationdate", nullable = false)
     private Instant creationDate;
 
+    @Column(name = "accountstatus", nullable = false)
     private boolean accountStatus;
 
 
